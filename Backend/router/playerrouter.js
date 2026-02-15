4aae88661d07100d577b5e920d2c251c158bb780
Dolisha Gandhi <dolishagandhi@gmail.com>
2026-02-15 18:23:59 +0530

    Add files via upload

--- Backend/router/playerrouter.js
+++ Backend/router/playerrouter.js
@@ -1,21 +1,22 @@
 let express=require("express");
 let router=express.Router();
 let auth=require("../middleware/player");
-const { playerregister, playerlogin } = require("../controller/playercont");
+const { playerregister, playerlogin, playerprofile, playerupdate } = require("../controller/playercont");
 
-// const multer = require("multer");
-// let upload =multer({ 
-//     storage:multer.diskStorage({
-//         destination:(req, file, cb)=>{
-//             cb(null,"./public/images");
-//         },
-//         filename:(req,file,cb)=>{
-//             cb(null, file.originalname);
-//         }
-//     })
-// })
+const multer = require("multer");
+let upload =multer({ 
+    storage:multer.diskStorage({
+        destination:(req, file, cb)=>{
+            cb(null,"./public/images");
+        },
+        filename:(req,file,cb)=>{
+            cb(null, file.originalname);
+        }
+    })
+})
 
 router.post("/playerregister",playerregister);
 router.post("/playerlogin",playerlogin);
-
+router.get("/playerprofile",auth,playerprofile);
+router.put("/playerupdate",auth,playerupdate);
 module.exports=router;
